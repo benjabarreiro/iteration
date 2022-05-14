9f4302238d8a2b20dd262c2aa08d968b14cce399
barreirobenjamin <barreirobenjamin@gmail.com>
2022-05-13 22:34:53 -0300

    google scripts

--- pages/_app.js
+++ pages/_app.js
@@ -45,7 +45,54 @@ function MyApp({ Component, pageProps }) {
         />`,
           }}
         />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-2G8CZ52F27"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-2G8CZ52F27');`,
+          }}
+        />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-228159306-1"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-228159306-1');`,
+          }}
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KKGW7BF');`,
+          }}
+        />
       </Head>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-KKGW7BF"
+          height="0"
+          width="0"
+          style="display:none;visibility:hidden"
+        ></iframe>
+      </noscript>
+
       <Navbar />
       <Component {...pageProps} />
       <Footer />
